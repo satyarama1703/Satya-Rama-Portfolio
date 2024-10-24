ba95fafd39ff846ece54ab7359dc71f59333d86d
Satya Rama Subrahmanyam Tallapragada <111050423+satyarama1703@users.noreply.github.com>
2024-10-25 02:20:55 +0530

    Add files via upload

---
+++ My_Portfolio-main/assets/js/main.js
@@ -0,0 +1,90 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeExperience(
+  ".digitalhouse",
+  `I have a strong background in web development, with expertise in building responsive and user-friendly websites using top web technologies in the market. I'm not only experienced but also passionate about this field, and I'm always excited to learn and develop websites. `,
+  "Frontend, UI-UX & backend",
+  "Experienced",
+  "Since 2021"
+);
+
+hoverChangeExperience(
+  ".zuplae",
+  `My expertise in mobile app development covers both iOS and Android platforms. I've worked on projects using multiple frameworks and platforms, allowing me to adapt and create solutions that cater to diverse user needs. I have a natural curiosity and an eagerness to learn more in this ever-evolving domain.`,
+  "Android and iOS",
+  "Hard Skilled",
+  "Since 2021"
+);
+
+hoverChangeExperience(
+  ".codigofontetv",
+  `Beyond my core skills, I'm an explorer of new technologies, enthusiastic about mobile and web technologies, and adept at delivering software solutions. I also bring soft skills like time management, problem-solving, and creative thinking to every project.`,
+  "SoftSkills and more...",
+  "True nature",
+  "Long Ago..."
+);
+
+hoverChangeExperience(
+  ".contweb",
+  `In the realm of content creation, my passion for design is evident. My content creation abilities extend to tools like Figma, Adobe, Photoshop, Flimora, & Canva,  allowing me to craft visually captivative content.`,
+  "Photo, Video Editing & Designing",
+  "Creative & Skilled",
+  "Since 2019"
+);
+
+hoverChangeDescription(
+  ".html",
+  "JAVA is my primary programming language that I have used to learn Data Structures and Algorithms, OOPS."
+);
+hoverChangeDescription(
+  ".css",
+  "Python is the first programming language I learnt in my journey & also I used python for fundamentals of data analytics."
+);
+hoverChangeDescription(
+  ".js",
+  "To dive deep into Database Management Systems I have learned the basic fundamentals of Structured Query Language."
+);
+hoverChangeDescription(
+  ".sass",
+  "Flutter is a good skill to have coz we can create both Android & Ios from the same code so I am exploring flutter further."
+);
+hoverChangeDescription(
+  ".react",
+  "HTML is the skeleton of any website so it is nessesary to have knowledge in this, I have a good amount of experience in this."
+);
+hoverChangeDescription(
+  ".next",
+  "CSS is used to to define styles for your web pages, I created some stunning pages using css's magic art and frameworks."
+);
+hoverChangeDescription(
+  ".styled",
+  "JavaScript is used to  to make web pages interactive, there is so much more to learn about JS and I am working on it."
+);
+hoverChangeDescription(
+  ".tailwind",
+  "PHP is used to create Dynamic content that interacts with databases, I have worked with php on my college labs."
+);
+hoverChangeDescription(
+  ".typescript",
+  "XML stores and transfers data, I used it while learing about web tech in my college and also while developing application on android studio."
+);
+hoverChangeDescription(
+  ".radix",
+  "Firebase is a NoSQL database that stores and syncs data in real-time, currently I am exploring Firebase and using it to build my mobile applications using flutter."
+);
+hoverChangeDescription(
+  ".cypress",
+  "I have learnt to use Android Studio in my college and built some really good android applications" 
+);
+hoverChangeDescription(
+  ".storybook",
+  "Bootstrap is a front-end framework known for its compatibility across browsers, I used BS to build my web pages and for my college work."
+);
